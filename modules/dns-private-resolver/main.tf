8d666b129131c5047b9278748bd7c49a7c22a1fc
gutt02 <sven.guttmann@gmail.com>
2023-02-21 18:39:31 +0100

    refactor

--- modules/dns-private-resolver/main.tf
+++ modules/dns-private-resolver/main.tf
@@ -20,23 +20,3 @@ data "azurerm_client_config" "client_config" {
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/subscription
 data "azurerm_subscription" "subscription" {
 }
-
-# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/private_dns_resolver
-resource "azurerm_private_dns_resolver" "this" {
-  name                = "${var.project.customer}-${var.project.name}-${var.project.environment}-dnspr"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  virtual_network_id  = var.virtual_network_id
-}
-
-# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/private_dns_resolver_inbound_endpoint
-resource "azurerm_private_dns_resolver_inbound_endpoint" "this" {
-  name                    = "${var.project.customer}-${var.project.name}-${var.project.environment}-dnspr-ibe"
-  private_dns_resolver_id = azurerm_private_dns_resolver.this.id
-  location                = var.location
-
-  ip_configurations {
-    private_ip_allocation_method = "Dynamic"
-    subnet_id                    = var.subnet_dnspr_inbound_id
-  }
-}
