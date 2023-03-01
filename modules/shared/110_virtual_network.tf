03d7acfec6b275e45440ad554e88a69b94364bee
gutt02 <sven.guttmann@gmail.com>
2023-03-01 19:41:55 +0100

    refactor

--- modules/shared/110_virtual_network.tf
+++ modules/shared/110_virtual_network.tf
@@ -1,10 +1,3 @@
-# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
-resource "azurerm_resource_group" "this" {
-  name     = "${var.project.customer}-${var.project.name}-${var.project.environment}-rg-network"
-  location = var.location
-  tags     = var.tags
-}
-
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_network
 resource "azurerm_virtual_network" "this" {
   name                = "${var.project.customer}-${var.project.name}-${var.project.environment}-vnet"
